47c1b11d88391b666f07fad6462340fbb134a14e
fadhailabima <88726104+fadhailabima@users.noreply.github.com>
2022-03-13 19:04:36 +0700

    Add files via upload

---
+++ Praktikum 3/soalnomor9.c
@@ -0,0 +1,60 @@
+//Nama File 	: CekBulan
+//Nama   	    : Fadhail Athaillah Bima
+//Nim           : 24060121140172
+//Kelas         : B2
+//Deskripsi 	: Menampilkan nama bulan dari nomor bulan yang dimasukkan pada layar
+
+
+#include<stdio.h>
+
+int main ()
+{
+    //kamus
+    int B;
+    //algoritma
+    printf("Pengecekan Bulan \n");
+    printf("masukkan nomor bulan : ");
+    scanf ( "%d", &B);
+
+    switch (B) {
+        case 1 :
+            printf("ini adalah bulan Januari!");
+            break;
+        case 2 :
+            printf("ini adalah bulan Februari!");
+            break;
+        case 3 :
+            printf("ini adalah bulan Maret!");
+            break;
+        case 4 :
+            printf("ini adalah bulan April!");
+            break;
+        case 5 :
+            printf("ini adalah bulan Mei!");
+            break;
+        case 6 :
+            printf("ini adalah bulan Juni!");
+            break;
+        case 7 :
+            printf("ini adalah bulan Juli!");
+            break;
+        case 8 :
+            printf("ini adalah bulan Agustus!");
+            break;
+        case 9 :
+            printf("ini adalah bulan September!");
+            break;
+        case 10 :
+            printf("ini adalah bulan Oktober!");
+            break;
+        case 11 :
+            printf("ini adalah bulan November!");
+            break;
+        case 12 :
+            printf("ini adalah bulan Desember!");
+            break;
+        default:
+        printf("Masukkan nomor bulan tidak tepat");
+        }
+    return 0;
+}
