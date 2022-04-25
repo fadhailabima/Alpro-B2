d2713dd011cb0b3786b8c18b303b11d4377de38a
fadhailabima <88726104+fadhailabima@users.noreply.github.com>
2022-04-25 17:11:27 +0700

    Add files via upload

---
+++ Praktikum 6/main.c
@@ -0,0 +1,28 @@
+/*Nama Program : Tetris.c*/
+/*Deskripsi    : menampilkan susunan karakter asterisk*/
+/*Pembuat      : Fadhail Athaillah Bima - 24060121140172*/
+/*Tgl Pembuatan : Rabu. 30 Maret 2022*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   // Kamus
+   int N,i,j;
+
+   // Algoritma
+   printf("Masukkan jumlah baris bintang yang diinginkan : ");
+   scanf("%d",&N);
+   for(i=1;i<=N;i++){                 //{Loop Baris}
+       for(j=1;j<=i;j++){             //{Loop Kolom}
+            printf("*");
+            if(j==i){
+                printf("  --%d",j);
+            }
+       }
+       printf("\n");
+    }
+   return 0;
+}
